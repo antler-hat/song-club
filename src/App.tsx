af9af82c0d07c46765577b0e61232eb5daf9450a
antler-hat <dbusheikin@gmail.com>
2025-08-09 00:07:28 -0400

    Set up for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <BrowserRouter basename="/song-club">
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/auth" element={<Auth />} />
