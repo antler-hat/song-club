5e3c1b94a80110f50e3060b35154ecbd7aff0a7d
antler-hat <dbusheikin@gmail.com>
2025-08-12 17:01:31 -0400

    Undo my deployment change

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: "/song-club/",
   server: {
     host: "::",
     port: 8080,
