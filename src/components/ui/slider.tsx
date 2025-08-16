996daf91dc54694b3fdb609974477701bb207a09
antler-hat <dbusheikin@gmail.com>
2025-08-16 17:45:42 -0400

    update slider style

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -15,7 +15,7 @@ const Slider = React.forwardRef<
     )}
     {...props}
   >
-    <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-secondary">
+    <SliderPrimitive.Track className="slider-track relative w-full grow overflow-hidden rounded-full">
       <SliderPrimitive.Range className="absolute h-full bg-primary" />
     </SliderPrimitive.Track>
     <SliderPrimitive.Thumb className="slider-thumb" />
